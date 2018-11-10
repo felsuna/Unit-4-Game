259b5be94769a15665262db687552cccec48274f
felsuna <felicia.sunanto@gmail.com>
2018-11-10 10:00:03 -0700

    conditionals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
-var computerChoice = '';
+var computerChoice = Math.floor(Math.random() * 102) + 19;;
 // var userOption = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
-var userGuess = 0;
+var crystalNumber = 0;
 var crystalArray = [
     'assets/images/Aquamarine.png',
     'assets/images/Citrine.png',
@@ -15,7 +15,7 @@ $("#computer-choice").text(computerChoice);
 $("#user-wins").text("Wins: " + wins);
 $("#user-losses").text("Losses: " + losses);
 $("#total-score").text("Your total score: " + totalScore);
-$("#user-guesses").text(userGuess);
+$("#crystal-number").text(crystalNumber);
 
 //Assigning random number to images, between 1 and 12.
 for (var i = 0; i < crystalArray.length; i++) {
@@ -26,34 +26,35 @@ for (var i = 0; i < crystalArray.length; i++) {
     $('#crystals').append(newCrystal);
 }
 
-//Random number generated for the computer's choice.
-if (computerChoice === '') {
-    var random = Math.floor(Math.random() * 102) + 19;
-    $("#computer-choice").text(computerChoice + random);
-    console.log(random, "Computer's choice");
-}
+$("#computer-choice").text("Cp Choice" + computerChoice);
+
 
 //Crystal click function, and also adds the integer each time a crystal is clicked.
 $('.crystals').on('click', function () {
     var crystalValue = parseInt($(this).attr('data-random'));
     totalScore += crystalValue;
     $("#total-score").text("Your total score: " + totalScore);
-    $("#user-guesses").text(userGuess + crystalValue);
-    console.log(totalScore,"Total Score");
-    console.log(crystalValue, "Crystal Value");
+    $("#crystal-number").text(crystalNumber + crystalValue);
 
     if (totalScore === computerChoice){
         wins++;
         totalScore = 0;
-        userGuess = 0;
+        crystalNumber = 0;
         crystalArray = [];
-        computerChoice = '';
-        console.log(wins,"Win");
+        computerChoice = Math.floor(Math.random() * 102) + 19;
+        $("#computer-choice").text("Cp Choice" + computerChoice);
+        $("#user-wins").text("Wins: " + wins);       
     }
-    else if (totalScore >= computerChoice){
-        
+    else if (totalScore > computerChoice){
+        totalScore = 0;
+        losses++;
+        crystalNumber = 0;
+        crystalArray = [];
+        computerChoice = Math.floor(Math.random() * 102) + 19;
+        $("#computer-choice").text("Cp Choice" + computerChoice);
+        $("#user-losses").text("Losses: " + losses); 
     }
-      
+    
 })
-
+ 
 
